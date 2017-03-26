ec832293f3ecd58bdfb83d3e4f18b4bcb5e51963
Yun Teng <mindtracker.yun24@gmail.com>
2017-03-25 22:28:13 -0400

    update pages

--- cheese/__init__.py
+++ cheese/__init__.py
@@ -26,9 +26,19 @@ def create_app(config, debug=False, testing=False, config_overrides=None):
     def index():
         return render_template('index.html')
 
+
     @app.route('/profiles')
     def profiles():
         return render_template('profiles.html')
+
+
+    @app.route('/services')
+    def services():
+        return render_template('profiles.html')
+
+    @app.route('/vision')
+    def vision():
+        return render_template('vision.html')
 
     # Add an error handler. This is useful for debugging the live application,
     # however, you should disable the output of the exception for production
