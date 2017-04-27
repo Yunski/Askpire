5e253c650e433843a8b6c97063d0c34ea3bdc7bc
Yun Teng <mindtracker.yun24@gmail.com>
2017-04-27 13:52:11 -0400

    add docker and change images

---
+++ docker_set_cert.sh
@@ -0,0 +1,5 @@
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://master4g3.cs-cn-beijing.aliyun.com:13572"
+export DOCKER_CERT_PATH=~/.acs/certs/askpire
+
+
--- main.py
+++ main.py
@@ -7,4 +7,4 @@ app = cheese.create_app(config)
 # This is only used when running locally. When running live, gunicorn runs
 # the application.
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
