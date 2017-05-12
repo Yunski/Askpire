d7048312cc19354d2f22e6079964a3ba4dcdb5d7
Yun Teng <mindtracker.yun24@gmail.com>
2017-05-12 00:18:11 -0400

    change contact page UI

--- cheese/static/js/contact.js
+++ cheese/static/js/contact.js
@@ -24,43 +24,10 @@ $(window).scroll(function() {
     }
 });
 
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
-
  // Additional, potentially unnecessary code
 $(document).ready(function () {
-    $(window).on('load scroll', function () {
-        var scrolled = $(this).scrollTop();
-        $('#title').css({
-            'transform': 'translate3d(0, ' + -(scrolled * 0.2) + 'px, 0)', // parallax (20% scroll rate)
-            'opacity': 1 - scrolled / 400 // fade out at 400px from top
-        });
-        $('#hero-vid').css('transform', 'translate3d(0, ' + -(scrolled * 0.25) + 'px, 0)'); // parallax (25% scroll rate)
-    });
-
-    // video controls
-    $('#state').on('click', function () {
-        var video = $('#hero-vid').get(0);
-        var icons = $('#state > span');
-        $('#overlay').toggleClass('fade');
-        if (video.paused) {
-            video.play();
-            icons.removeClass('iconicfill-play').addClass('iconicfill-pause');
-        } else {
-            video.pause();
-            icons.removeClass('iconicfill-pause').addClass('iconicfill-play');
-        }
+    $("#contact-form").submit(function(e) {
+        e.preventDefault();
+        document.location.href="/contact";
     });
 });
