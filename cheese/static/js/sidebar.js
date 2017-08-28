d0eb7e1a66200bb2e6c677caac90568fb053e0ee
Yun Teng <mindtracker.yun24@gmail.com>
2017-08-28 16:13:01 -0700

    rename folder

--- cheese/static/js/sidebar.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    var tabs = {"dashboard": 1, "profile": 2, "schedule": 3, "notifications": 4, "calendar": 3};
-    var path = window.location.pathname.split("/")[1];
-    $("#sidebar-nav li").removeClass("active");
-    $("#sidebar-nav li:nth-child(" + tabs[path] + ")").addClass("active");
-});
